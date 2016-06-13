65daa0d37f2ae46b4898dc22a1e05c3c911abf72
Jason Meridth <Jason.Meridth@rackspace.com>
2016-06-13 01:02:11 -0500

    Drop all contact stuff.

--- meridthio.py
+++ meridthio.py
@@ -2,15 +2,9 @@ import os
 import re
 
 from datetime import date
-from flask import Flask, render_template, request
-from flask_mail import Mail, Message
-from flask_recaptcha import ReCaptcha
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-site_key = os.environ['MERIDTH_IO_RECAPTCHA_SITEKEY']
-secret_key = os.environ['MERIDTH_IO_RECAPTCHA_SECRETKEY']
-recaptcha = ReCaptcha(app=app, site_key=site_key, secret_key=secret_key)
 
 
 def is_email_address_valid(email):
@@ -28,26 +22,7 @@ def home():
     years_experience = current_year - 2000
     return render_template('index.html',
                            current_year=current_year,
-                           years_experience=years_experience,
-                           recaptcha=recaptcha.get_code())
-
-
-@app.route("/contact", methods=["POST"])
-def contact():
-    if (not request.form['name'] or not request.form['email'] or
-        not request.form['phont'] or not request.form['message'] or
-            not is_email_address_valid(request.form['email'])):
-        return False
-
-    if recaptcha.verify():
-        mail = Mail(app)
-        msg = Message("",
-                      sender=('Meridth.io website', 'noreply@meridth.io'),
-                      recipients=['jmerdith@gmail.com'],
-                      body=request.form['contact_email'])
-        mail.send(msg)
-    else:
-        error(401)
+                           years_experience=years_experience)
 
 
 @app.errorhandler(404)
